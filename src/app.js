cbab27abfc0f4d86574c615d6de8758f4d34edf9
toaonly <toaonly42@gmail.com>
2024-04-09 01:32:52 +0900

    init

--- src/app.js
+++ src/app.js
@@ -5,11 +5,4 @@ export default async function renderApp() {
   const app = document.querySelector('#app')
 
   app.append(renderUsers(await api.getUsers()))
-  // app.innerHTML = `
-  //   <h1>사용자 목록</h1>
-
-  //   <div>
-  //     <button class="btn-start">start</button>
-  //   </div>
-  // `
 }
